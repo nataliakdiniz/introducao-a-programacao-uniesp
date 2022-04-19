57d3eb63f41cec49d9d1f8fbad296ff15e77c2f1
Natália Diniz <98323720+nataliakdiniz@users.noreply.github.com>
2022-04-18 21:15:06 -0300

    Delete question_02.py

--- question_02.py
+++
@@ -1,9 +0,0 @@
-# Faça um programa que mostre as tabuadas dos números de 1 a 10.
-
-number = int(input("Qual o número que quer que se faça a tabuada? "))
-for valor in range(0,11):
-    resultado = valor * number
-    print("{} x {} = {}".format(number, valor, resultado))
-
-
-
